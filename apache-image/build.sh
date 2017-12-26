12c4dd84d88012d19dc4d5373fceb993fe759513
Yahav Biran <ybiran@colostate.edu>
2017-12-25 20:24:54 -0800

    Support for Apache SSL Self Signed

---
+++ apache-image/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ver=0.7
+image=`docker build . | grep "Successfully built"| awk '{print $NF}'`
+if [ -z "$image" ]
+then
+  echo "error in docker build"
+  exit
+fi
+docker tag $image us.gcr.io/ananware-186718/apache-ssl:$ver
+gcloud docker -- push us.gcr.io/ananware-186718/apache-ssl:$ver
+helm delete `helm list | egrep -v 'nginx' | grep -v NAME|awk '{print $1}'`
+cd ../
+helm install ./
